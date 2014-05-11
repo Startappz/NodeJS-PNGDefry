1d95401a3f632418f2f5b7ab030f6329ee49679d
Mutaz Salahat <mutaz.startappz.com>
2014-05-12 00:46:05 +0300

    adding test direcotry

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,9 @@
 var PNGDDefry = require("../lib");
+var fs = require("fs");
+
+if (!fs.existsSync(__dirname + "/output")) {
+    fs.mkdirSync(__dirname + "/output", '777');
+}
 
 var png_converter = new PNGDDefry();
 png_converter.convert(__dirname + "/AppIcon57x57.png",
